7dc886c5b924094e9ee4ed791420354119f3959b
KeithCoke <littlecokers@gmail.com>
2022-11-14 21:43:49 -0800

    additional js

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,29 @@
+
 document.getElementById('modalBtn').addEventListener('click',
 function(){
-document.querySelector('.modal-container').style.display = "flex";
+document.querySelector('.modal').style.display = "flex";
 });
 
-document.getElementById('closeBtn').addEventListener('click',
-function(){
-document.querySelector('.modal-container').style.display = "none";
-});
+// document.getElementById('close').addEventListener('click',
+// function(){
+// document.querySelector('.modal').style.display = "none";
+// });
+// var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+var modal = document.getElementById("modal");
 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
 
 function revealMessage() {
   document.getElementById("hiddenMessage").style.display = 'block';
