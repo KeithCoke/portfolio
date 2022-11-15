64632fe6fab24d20c662d062de269714e768aa7f
KeithCoke <littlecokers@gmail.com>
2022-11-14 21:45:33 -0800

    additional js resubmission

--- portfolio.js
+++ portfolio.js
@@ -4,26 +4,12 @@ function(){
 document.querySelector('.modal').style.display = "flex";
 });
 
-// document.getElementById('close').addEventListener('click',
-// function(){
-// document.querySelector('.modal').style.display = "none";
-// });
-// var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 var modal = document.getElementById("modal");
-
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
 
 span.onclick = function() {
   modal.style.display = "none";
 }
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
 
 function revealMessage() {
   document.getElementById("hiddenMessage").style.display = 'block';
