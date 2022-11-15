7bfe9719468e5bfab615b05c41701cb1d2670a5e
KeithCoke <littlecokers@gmail.com>
2022-11-14 20:27:49 -0800

    additional js

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,27 @@
+document.getElementById('modalBtn').addEventListener('click',
+function(){
+document.querySelector('.modal-container').style.display = "flex";
+});
+
+document.getElementById('closeBtn').addEventListener('click',
+function(){
+document.querySelector('.modal-container').style.display = "none";
+});
+
+
 function revealMessage() {
-    document.getElementById("hiddenMessage").style.display = 'block';
+  document.getElementById("hiddenMessage").style.display = 'block';
 }
 
 function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+  var element = document.body;
+  element.classList.toggle("dark-mode");
 }
 
 function revealProjects() {
-    document.getElementById("hiddenProject").style.display = 'block';
+  document.getElementById("hiddenProject").style.display = 'block';
 }
+
+
+
 
